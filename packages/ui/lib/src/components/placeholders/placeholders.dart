49fd30b2a374b416c27112d5e6e05929b5690b10
honor <honor864510@gmail.com>
2025-04-04 11:26:42 +0500

    feat(ui): add shared UI components, theme, and shader-based shimmer effect
    Introduce a new `ui` package containing shared UI components, theme configurations, and a shader-based shimmer effect for placeholders. The package includes:
    - A `Shimmer` widget for loading placeholders with customizable parameters.
    - Light and dark theme configurations with color schemes.
    - Exported components and theme extensions for easy integration.
    - Updated dependencies in `pubspec.yaml` files to ensure compatibility.

---
+++ packages/ui/lib/src/components/placeholders/placeholders.dart
@@ -0,0 +1,2 @@
+export 'package:ui/src/components/placeholders/shimmer.dart';
+export 'package:ui/src/components/placeholders/text_placeholder.dart';
---
+++ packages/ui/lib/ui.dart
@@ -0,0 +1,2 @@
+export 'src/components/components.dart';
+export 'src/theme/theme.dart';
